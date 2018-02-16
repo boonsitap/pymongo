1f9820f6127c8fa7fd1812a25039696fab2da64c
Boonsita Pornin <35695097+boonsitap@users.noreply.github.com>
2018-02-16 16:35:38 +0700

    Update login.py

--- login.py
+++ login.py
@@ -4,6 +4,7 @@ from flask import Flask,request
 from flask_restful import Resource ,Api ,reqparse
 
 client = pymongo.MongoClient('localhost',27017)
+db = client.company
 
 app= Flask(__name__)
 api = Api(app)
@@ -13,40 +14,39 @@ parser.add_argument('username')
 parser.add_argument('password')
 parser.add_argument('firstname')
 parser.add_argument('lastname')
-parser.add_argument('employee_number')
-
-db = client.db_example1
+parser.add_argument('employee_id')
 
 work = db.work
+
 class History(Resource):
         def get(self):
                 args = parser.parse_args()
                 id = args['id']
-                data = work.find_one({"user.employee_number":id})
+                data = work.find_one({"user.employee_id":id})
                 if(data):
                         firstname = data['user']['firstname']
                         lastname = data['user']['lastname']
                         list_work = data['list_work']
                         return {"firstname":firstname,"lastname":lastname,"list_work":list_work}
                 return {}
-class Registration(Resource):
+class Register(Resource):
         def post(self):
                 args = parser.parse_args()
-                id = args['employee_number']
+                id = args['employee_id']
                 firstname = args['firstname']
                 lastname = args['lastname']
                 password = args['password']
-                data = work.find_one({"user.employee_number":id})
+                data = work.find_one({"user.employee_id":id})
                 if(data):
                         return {"err":"has this id"}
                 work.insert({"user":{"employee_number":id,"firstname":firstname,"lastname":lastname,"password":password},"list_work":[]})
-                return {"firstname":firstname,"lastname":lastname,"employee_number":id,"password":password}
+                return {"firstname":firstname,"lastname":lastname,"employee_id":id,"password":password}
 class Login(Resource):
         def post(self):
                 args = parser.parse_args()
                 username = args['username']
                 password = args['password']
-                data = work.find_one({"user.employee_number":username,"user.password":password})
+                data = work.find_one({"user.employee_id":username,"user.password":password})
                 if(data):
                         firstname = data['user']['firstname']
                         lastname = data['user']['lastname']
@@ -54,7 +54,7 @@ class Login(Resource):
                         work.update({"user.employee_number":username},{"$push":{"list_work":{"datetime":datetime_login}}})
                         return {"firstname":firstname,"lastname":lastname,"datetime":datetime_login}
                 return {}
-api.add_resource(Registration,'/api/register')
+api.add_resource(Register,'/api/register')
 api.add_resource(Login,'/api/login')
 api.add_resource(History,'/api/list_work')
 
