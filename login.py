0a60a53eef09a7e8d3b43886ecb4169749b5ff85
Boonsita Pornin <35695097+boonsitap@users.noreply.github.com>
2018-02-16 16:44:28 +0700

    Update login.py

--- login.py
+++ login.py
@@ -16,18 +16,18 @@ parser.add_argument('firstname')
 parser.add_argument('lastname')
 parser.add_argument('employee_id')
 
-work = db.work
+office = db.office
 
 class History(Resource):
         def get(self):
                 args = parser.parse_args()
                 id = args['id']
-                data = work.find_one({"user.employee_id":id})
+                info = office.find_one({"user.employee_id":id})
                 if(data):
-                        firstname = data['user']['firstname']
-                        lastname = data['user']['lastname']
-                        list_work = data['list_work']
-                        return {"firstname":firstname,"lastname":lastname,"list_work":list_work}
+                        firstname = info['user']['firstname']
+                        lastname = info['user']['lastname']
+                        list_office = info['list_office']
+                        return {"firstname":firstname,"lastname":lastname,"list_office":list_office}
                 return {}
 class Register(Resource):
         def post(self):
@@ -36,27 +36,27 @@ class Register(Resource):
                 firstname = args['firstname']
                 lastname = args['lastname']
                 password = args['password']
-                data = work.find_one({"user.employee_id":id})
+                data = office.find_one({"user.employee_id":id})
                 if(data):
                         return {"err":"has this id"}
-                work.insert({"user":{"employee_number":id,"firstname":firstname,"lastname":lastname,"password":password},"list_work":[]})
+                office.insert({"user":{"employee_number":id,"firstname":firstname,"lastname":lastname,"password":password},"list_office":[]})
                 return {"firstname":firstname,"lastname":lastname,"employee_id":id,"password":password}
 class Login(Resource):
         def post(self):
                 args = parser.parse_args()
                 username = args['username']
                 password = args['password']
-                data = work.find_one({"user.employee_id":username,"user.password":password})
+                data = office.find_one({"user.employee_id":username,"user.password":password})
                 if(data):
-                        firstname = data['user']['firstname']
-                        lastname = data['user']['lastname']
+                        firstname = info['user']['firstname']
+                        lastname = info['user']['lastname']
                         datetime_login = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-                        work.update({"user.employee_number":username},{"$push":{"list_work":{"datetime":datetime_login}}})
+                        office.update({"user.employee_number":username},{"$push":{"list_office":{"datetime":datetime_login}}})
                         return {"firstname":firstname,"lastname":lastname,"datetime":datetime_login}
                 return {}
 api.add_resource(Register,'/api/register')
 api.add_resource(Login,'/api/login')
-api.add_resource(History,'/api/list_work')
+api.add_resource(History,'/api/list_office')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5000)
